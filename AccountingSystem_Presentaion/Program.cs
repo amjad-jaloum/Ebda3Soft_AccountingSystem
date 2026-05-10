ba9fe2ba4ce807f3fe3e93472e289d0efd9ffab5
Amjad <amjadjaloom720@gmail.com>
2026-05-10 19:12:23 +0300

    Initial Commit: 3-Tier Architecture structure and Database script

---
+++ AccountingSystem_Presentaion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ebda3Soft_AccountingSystem
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+}
